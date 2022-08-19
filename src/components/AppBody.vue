22bc6fd929dcd07c3ea8e505cb005706fc0906fc
Carlo Munguia <cmunguia@denimgroup.com>
2022-08-18 21:08:03 -0500

    Literal 2
     dead simple component update

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,3 +1,9 @@
 <template>
-  Body
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img alt="dali books" src="./../assets/booksglasses.png" class="img-fluid mt-5" />
+      </div>
+    </div>
+  </div>
 </template>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,3 +1,14 @@
 <template>
-  Footer
+  <footer class="d-flex align-items-center justify-content-center mt-5">
+      Copyright &copy; {{ new Date().getFullYear() }}
+  </footer>
 </template>
+
+<style scoped>
+  footer {
+    background: black;
+    color: white;
+    min-height: 3em;
+    font-size: 0.8em;
+  }
+</style>
