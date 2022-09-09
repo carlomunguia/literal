981d3e34c60eceda09721eaf0f0ad787b7b080cc
Carlo Munguia <cmunguia@denimgroup.com>
2022-09-08 22:17:44 -0500

    Literal 23
    added a protected route & new authtoken middleware

---
+++ src/cmd/api/middleware.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func (app *application) AuthTokenMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := app.models.Token.AuthenticateToken(r)
+		if err != nil {
+			payload := jsonResponse{
+				Error:   true,
+				Message: "invalid auth credentials",
+			}
+
+			_ = app.writeJSON(w, http.StatusUnauthorized, payload)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
--- src/cmd/api/routes.go
+++ src/cmd/api/routes.go
@@ -26,6 +26,19 @@ func (app *application) routes() http.Handler {
 	mux.Post("/users/login", app.Login)
 	mux.Post("/users/logout", app.Logout)
 
+	mux.Route("/admin", func(mux chi.Router) {
+		mux.Use(app.AuthTokenMiddleware)
+
+		mux.Post("/foo", func(w http.ResponseWriter, r *http.Request) {
+			payload := jsonResponse{
+				Error:   false,
+				Message: "bar",
+			}
+
+			app.writeJSON(w, http.StatusOK, payload)
+		})
+	})
+
 	mux.Get("/users/all", func(w http.ResponseWriter, r *http.Request) {
 		var users data.User
 		all, err := users.GetAll()
