88f1ae45dad9b67914fafe294891f49ff35c5f91
Carlo Munguia <cmunguia@denimgroup.com>
2022-09-12 22:18:58 -0500

    Literal 27
    created user table / display list

--- src/components/AppUsers.vue
+++ src/components/AppUsers.vue
@@ -4,6 +4,27 @@
       <div class="col">
         <h1 class="mt-3">All Users</h1>
       </div>
+
+      <hr />
+
+      <table class="table table-compact table-striped">
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="u in this.users" v-bind:key="u.id">
+            <td>
+              <router-link :to="`/admin/users/${u.id}`"
+                >{{ u.last_name }}, {{ u.first_name }}</router-link
+              >
+            </td>
+            <td>{{ u.email }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
