bf0360ead6d881cb2e77b7ab8b5afaf28bed311f
Carlo Munguia <cmunguia@denimgroup.com>
2022-10-02 14:40:17 -0500

    Literal 46
    added proper add/edit forms to add/edit books, brought in data / loading image JS functions

--- src/components/BookEdit.vue
+++ src/components/BookEdit.vue
@@ -11,6 +11,57 @@
             class="img-fluid img-thumbnail book-cover"
             alt="cover" />
         </div>
+
+        <div class="mb-3">
+          <label for="formFile" class="form-label">Cover Image</label>
+          <input
+            v-if="this.book.id === 0"
+            ref="coverInput"
+            class="form-control"
+            type="file"
+            id="formFile"
+            @change="loadCoverImage" />
+          <input
+            v-else
+            ref="coverInput"
+            class="form-control"
+            type="file"
+            id="formFile"
+            @change="loadCoverImage" />
+        </div>
+
+        <text-input
+          v-model="book.title"
+          type="text"
+          required="true"
+          label="Title"
+          :value="book.title"
+          name="title"></text-input>
+
+        <select-input
+          name="author-id"
+          v-model="this.book.author_id"
+          :items="this.authors"
+          required="required"
+          label="Author"></select-input>
+
+        <text-input
+          v-model="book.publication_year"
+          type="text"
+          required="true"
+          label="Publication Year"
+          :value="book.publication_year"
+          name="publication-year"></text-input>
+
+        <div class="mb-3">
+          <label for="description" class="form-label">Description</label>
+          <textarea
+            required
+            v-model="book.description"
+            class="form-control"
+            id="description"
+            rows="3"></textarea>
+        </div>
       </form-tag>
     </div>
   </div>
@@ -21,16 +72,47 @@
   import FormTag from '@/components/forms/FormTag.vue'
   import TextInput from '@/components/forms/TextInput.vue'
   import SelectInput from '@/components/forms/SelectInput.vue'
-  import notie from 'notie'
+  // import notie from 'notie'
 
   export default {
     name: 'BookEdit',
     beforeMount() {
       Security.requireToken()
     },
+    components: {
+      'form-tag': FormTag,
+      'text-input': TextInput,
+      'select-input': SelectInput
+    },
+    data() {
+      return {
+        book: {
+          id: 0,
+          title: '',
+          author_id: 0,
+          publication_year: 0,
+          description: '',
+          cover: '',
+          slug: '',
+          genres: [],
+          genre_ids: []
+        },
+        authors: [],
+        imgPath: process.env.VUE_APP_LITERAL_IMAGE_URL,
+        genres: []
+      }
+    },
     methods: {
       submitHandler: function (data) {
         console.log(data)
+      },
+      loadCoverImage: function (event) {
+        const file = event.target.files[0]
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = () => {
+          this.book.cover = reader.result
+        }
       }
     }
   }
